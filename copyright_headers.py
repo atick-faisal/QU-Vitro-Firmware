933f2dc12cae75ec82daf7cca065cf3050ac7a04
Atick Faisal <atickfaisal@gmail.com>
2023-09-18 08:20:22 +0300

    Fix license

--- copyright_headers.py
+++ copyright_headers.py
@@ -4,19 +4,21 @@
 import sys
 
 header = """\
-// Copyright 2023 Atick Faisal
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ *  Copyright 2023 Atick Faisal
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 """
 
 fname = sys.argv[1]
@@ -56,7 +58,7 @@ header_done = False
 while line.strip() == "":  # Skip empty lines at the top
     line = fileread.readline()
 
-if line.find("// Copyright 2023 Atick Faisal") == -1:  # Godot header starts this way
+if line.find("/*") == -1:  # Godot header starts this way
     # Maybe starting with a non-Godot comment, abort header magic
     header_done = True
 
